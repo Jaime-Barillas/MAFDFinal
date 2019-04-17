5e1a2263a19dba025269aa81415d53518f9f3bc2
Jaime Barillas <jaimeenrique97@gmail.com>
2019-04-17 14:41:41 -0400

    S,L, & R total counts working. TODO: transaction totals per store.

--- Final/DataSplitCount/DataSplitCount.cbl
+++ Final/DataSplitCount/DataSplitCount.cbl
@@ -67,7 +67,109 @@
        01 ws-eof-flag                  pic x value 'n'.
 
        01 ws-trans-code                pic x value space.
-           88 ws-sale-or-layaway             value 'S', 'L'.
+           88 ws-trans-sale                  value 'S'.
+           88 ws-trans-layaway               value 'L'.
+           88 ws-trans-return                value 'R'.
+
+       01 ws-report-heading.
+           05 filler                   pic x(40)
+               value ' Counts And Control Totals -- Final Gr 8'.
+
+       01 ws-totals.
+           05 ws-tot-sl-rec            pic 9(4)
+               value 0.
+           05 ws-tot-sl-rec-amnt       pic 9(9)v99
+               value 0.
+           05 ws-tot-s-rec             pic 9(4)
+               value 0.
+           05 ws-tot-s-rec-amnt        pic 9(9)v99
+               value 0.
+           05 ws-tot-l-rec             pic 9(4)
+               value 0.
+           05 ws-tot-l-rec-amnt        pic 9(9)v99
+               value 0.
+           05 ws-tot-s-rec-ptc         pic 999v99
+               value 0.
+           05 ws-tot-l-rec-ptc         pic 999v99
+               value 0.
+           05 ws-tot-r-rec             pic 9(4)
+               value 0.
+           05 ws-tot-r-rec-amnt        pic 9(9)v99
+               value 0.
+
+       01 ws-td-sl-records.
+           05 filler                   pic x value space.
+           05 filler                   pic x(27)
+               value 'Total S&L Records:         '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-sl-rec             pic Z,ZZ9.
+
+       01 ws-td-sl-amount.
+           05 filler                   pic x value space.
+           05 filler                   pic x(18)
+               value 'Total S&L Amount: '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-sl-rec-amnt        pic ZZZ,ZZZ,ZZ9.99.
+
+       01 ws-td-s-records.
+           05 filler                   pic x value space.
+           05 filler                   pic x(27)
+               value 'Total S Records:           '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-s-rec              pic Z,ZZ9.
+
+       01 ws-td-s-amount.
+           05 filler                   pic x value space.
+           05 filler                   pic x(18)
+               value 'Total S Amount:   '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-s-rec-amnt         pic ZZZ,ZZZ,ZZ9.99.
+
+       01 ws-td-l-records.
+           05 filler                   pic x value space.
+           05 filler                   pic x(27)
+               value 'Total L Records:           '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-l-rec              pic Z,ZZ9.
+
+       01 ws-td-l-amount.
+           05 filler                   pic x value space.
+           05 filler                   pic x(18)
+               value 'Total L Amount:   '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-l-rec-amnt         pic ZZZ,ZZZ,ZZ9.99.
+
+       01 ws-td-s-percent.
+           05 filler                   pic x value space.
+           05 filler                   pic x(26)
+               value 'Percent S:                '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-s-rec-pct          pic ZZ9.9.
+           05 filler                   pic x
+               value '%'.
+
+       01 ws-td-l-percent.
+           05 filler                   pic x value space.
+           05 filler                   pic x(26)
+               value 'Percent R:                '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-l-rec-pct          pic ZZ9.9.
+           05 filler                   pic x
+               value '%'.
+
+       01 ws-td-r-records.
+           05 filler                   pic x value space.
+           05 filler                   pic x(27)
+               value 'Total R Records:           '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-r-rec              pic Z,ZZ9.
+
+       01 ws-td-r-amount.
+           05 filler                   pic x value space.
+           05 filler                   pic x(18)
+               value 'Total R Amount:   '.
+      *              0---+5+---1---+5+---2---+5+---3
+           05 ws-td-r-rec-amnt         pic ZZZ,ZZZ,ZZ9.99.
 
 
        procedure division.
@@ -80,8 +182,11 @@
            read input-file
                at end move 'y'         to ws-eof-flag.
 
-           perform 100-process-record
+           perform 100-print-headings.
+           perform 200-accumulate-totals
              until ws-eof-flag = 'y'.
+           perform 300-calculate-percentages.
+           perform 400-print-totals.
 
            close input-file,
                  sales-layaways-file,
@@ -90,20 +195,77 @@
            goback.
 
 
-       100-process-record.
+       100-print-headings.
+           write report-output         from ws-report-heading
+               before advancing 2 lines.
+
+
+       200-accumulate-totals.
            move in-trans-code          to ws-trans-code.
 
-           perform 500-print-record.
+           if (ws-trans-sale)
+               add 1                   to ws-tot-sl-rec
+               add in-trans-amnt       to ws-tot-sl-rec-amnt
+
+               add 1                   to ws-tot-s-rec
+               add in-trans-amnt       to ws-tot-s-rec-amnt
+           else if (ws-trans-layaway)
+               add 1                   to ws-tot-sl-rec
+               add in-trans-amnt       to ws-tot-sl-rec-amnt
+
+               add 1                   to ws-tot-l-rec
+               add in-trans-amnt       to ws-tot-l-rec-amnt
+           else if (ws-trans-return)
+               add 1                   to ws-tot-r-rec
+               add in-trans-amnt       to ws-tot-r-rec-amnt
+           end-if
+           end-if
+           end-if.
+
+      *    Lastly, we print the record to the appropriate file.
+           perform 250-print-record.
 
            read input-file
                at end move 'y'         to ws-eof-flag.
 
 
-       500-print-record.
-           if (ws-sale-or-layaway)
+       250-print-record.
+           if (ws-trans-sale or ws-trans-layaway)
                write sl-output         from input-record
            else
                write r-output          from input-record
            end-if.
+
+
+       300-calculate-percentages.
+           compute ws-tot-s-rec-ptc rounded =
+             (ws-tot-s-rec / ws-tot-sl-rec) * 100.
+           compute ws-tot-l-rec-ptc rounded =
+             (ws-tot-l-rec / ws-tot-sl-rec) * 100.
+
+
+       400-print-totals.
+           move ws-tot-sl-rec          to ws-td-sl-rec.
+           move ws-tot-sl-rec-amnt     to ws-td-sl-rec-amnt.
+           move ws-tot-s-rec           to ws-td-s-rec.
+           move ws-tot-s-rec-amnt      to ws-td-s-rec-amnt.
+           move ws-tot-l-rec           to ws-td-l-rec.
+           move ws-tot-l-rec-amnt      to ws-td-l-rec-amnt.
+           move ws-tot-r-rec           to ws-td-r-rec.
+           move ws-tot-r-rec-amnt      to ws-td-r-rec-amnt.
+           move ws-tot-s-rec-ptc       to ws-td-s-rec-pct.
+           move ws-tot-l-rec-ptc       to ws-td-l-rec-pct.
+
+           write report-output         from ws-td-sl-records.
+           write report-output         from ws-td-sl-amount.
+           write report-output         from ws-td-s-records.
+           write report-output         from ws-td-s-amount.
+           write report-output         from ws-td-l-records.
+           write report-output         from ws-td-l-amount.
+           write report-output         from ws-td-r-records.
+           write report-output         from ws-td-r-amount.
+           write report-output         from ws-td-s-percent.
+           write report-output         from ws-td-l-percent
+               before advancing 2 lines.
 
        end program DataSplitCount.
